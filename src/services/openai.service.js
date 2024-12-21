77659dc500f5669118d9d05fe3016954831cfba9
Ryan Poh <ranenpoh@gmail.com>
2024-12-21 23:13:52 +0800

    clean + change prompt

--- src/services/openai.service.js
+++ src/services/openai.service.js
@@ -10,7 +10,7 @@ const queryOpenAi = async text => {
         messages: [
             {role: 'system', content: 'You are a helpful assistant.'},
             {role: 'user', content: `Here is the text extracted from a PDF: ${text}`},
-            {role: 'user', content: 'Read the text and generate 10 Multiple Choice Questions in json format with the following variables 1. question 2. choice1 3. choice2 4. choice3 5. choice4 6. answer   "answer" is an integer from range 1 to 4, representing which choice is correct'}
+            {role: 'user', content: 'Read the text and generate 5 Multiple Choice Questions in json format (as a list of objects) with the following variables 1. question 2. choice1 3. choice2 4. choice3 5. choice4 6. answer   "answer" is an integer from range 1 to 4, representing which choice is correct'}
         ],
         model: 'gpt-4',
     });
